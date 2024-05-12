5bd524c04a60d3173377e7acfbd30a026e34edc2
ikromjon0102 <ikromjon01021995@gmail.com>
2024-05-12 22:50:24 +0500

    pagination & search books

--- books/views.py
+++ books/views.py
@@ -1,12 +1,35 @@
+from msilib.schema import ListView
+
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import View
 
 from books.models import Book
 
+# class BookListView(ListView):
+#     model = Book
+#     template_name = 'books/book_list.html'
+#     context_object_name = 'books'
+#     paginate_by = 4
+
+
+
+
 class BookListView(View):
     def get(self, request):
         books = Book.objects.all()
-        context = {'books': books}
+
+        search_query = request.GET.get('q')
+        if search_query:
+            books = books.filter(title__icontains=search_query)
+
+        page_size = request.GET.get('page_size', 3)
+        paginator = Paginator(books, page_size)
+
+        page_num = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_num)
+
+        context = {"page_obj": page_obj,}
         return render(request, 'books/list.html', context)
 
 class BookDetailView(View):
